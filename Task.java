94913bd674d5e919b027b11b14ec00f8986ed9ec
virpalkaur2605 <virpalkaur2605@gmail.com>
2024-05-20 11:56:38 +0530

    Update Task.java
    Task Class and constructor

--- Task.java
+++ Task.java
@@ -1,1 +1,25 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+class Task {
+    private String taskID;
+    private String title;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private String priority;
+    private String assignedUserID;
+    private String status; // "Ongoing", "Completed", "Pending"
+
+    public Task(String taskID, String title, String description, Date startDate, Date endDate, String priority, String assignedUserID, String status) {
+        this.taskID = taskID;
+        this.title = title;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.priority = priority;
+        this.assignedUserID = assignedUserID;
+        this.status = status;
+    }
+}
