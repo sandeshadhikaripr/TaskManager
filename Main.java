0b34c3a89296463f3a5d9202041df652b0128d4f
virpalkaur2605 <virpalkaur2605@gmail.com>
2024-05-21 08:24:21 +0530

    Update Main.java
    Switch case added

--- Main.java
+++ Main.java
@@ -27,6 +27,62 @@ public class Main {
             System.out.println("13. View completed tasks");
             System.out.println("14. Exit");
             System.out.print("Choose an option: ");
+                    int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                case 6:
+                    break;
+
+                case 7:
+                    break;
+
+                case 8:
+                    break;
+
+                case 9:
+                    break;
+
+                case 10:
+
+                    break;
+
+                case 11:
+                    break;
+
+                case 12:
+
+                    break;
+
+                case 13:
+
+                    break;
+
+                case 14:
+                    exit = true;
+                    System.out.println("Exiting...");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        
         }
+        scanner.close();
     }
 }
