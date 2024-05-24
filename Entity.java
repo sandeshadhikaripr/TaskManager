c60ec4f041986c4fed58156b164625d6e7a3c07b
sandeshadhikaripr <sandeshadhikaripr@gmail.com>
2024-05-24 21:20:26 +0530

    Create Entity.java
    Entity class

---
+++ Entity.java
@@ -0,0 +1,17 @@
+abstract class Entity {
+    private String id;
+
+    public Entity(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public abstract void displayDetails();
+}
